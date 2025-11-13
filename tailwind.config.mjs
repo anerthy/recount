5427b5243173b5c8a48129ffb9d67e9752758ad9
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-12 22:22:19 -0600

    feat: add initial Tailwind CSS configuration file

---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
+  // theme: {
+  //   extend: {
+  //     fontFamily: {
+  //       sans: ['Inter', 'sans-serif'],
+  //     },
+  //     colors: {
+  //       dark: '#1a202c',
+  //       light: '#f7fafc',
+  //     },
+  //   },
+  // },
+};
