fca985876a75406a0525deada5ec0cd808982ce3
anerthy <112020687+anerthy@users.noreply.github.com>
2025-11-13 09:39:04 -0600

    feat: update Tailwind CSS configuration with custom theme settings

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,22 @@
 export default {
   content: ['./src/**/*.{astro,js,ts,jsx,tsx}'],
-  // theme: {
-  //   extend: {
-  //     fontFamily: {
-  //       sans: ['Inter', 'sans-serif'],
-  //     },
-  //     colors: {
-  //       dark: '#1a202c',
-  //       light: '#f7fafc',
-  //     },
-  //   },
-  // },
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#13ecc8',
+        'background-light': '#f6f8f8',
+        'background-dark': '#10221f',
+      },
+      fontFamily: {
+        display: ['Manrope', 'sans-serif'],
+      },
+      borderRadius: {
+        DEFAULT: '0.5rem',
+        lg: '0.75rem',
+        xl: '1rem',
+        full: '9999px',
+      },
+    },
+  },
 };
